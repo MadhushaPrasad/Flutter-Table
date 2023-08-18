1be8d59a8525bf60b37859418c12d5a3bec0b0b2
MadhushaPrasad <madushaprasad21@gmail.com>
2023-08-18 12:46:29 +0530

    chore (custom-table component): Create a custom table component

---
+++ lib/widgets/custom-table.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CustomTable extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _CustomTableState();
+  }
+}
+
+class _CustomTableState extends State<CustomTable> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Custom Table'),
+    );
+  }
+}
